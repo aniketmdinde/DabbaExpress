a8348973e5087790db1506ecd2c9cdb0ad92bf4c
AvatarN03 <jsprashanth003@gmail.com>
2025-03-16 02:20:25 +0530

    location-route added

--- frontend/src/components/Dashboard.jsx
+++ frontend/src/components/Dashboard.jsx
@@ -1,6 +1,17 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 
 const Dashboard = () => {
+
+    useEffect(()=>{
+        window.navigator.geolocation.getCurrentPosition(
+            (position) => {
+                console.log(position);
+            },
+            (error) => {
+                console.error('Error fetching location', error);
+            }
+        )
+    }, [])
   return (
     <div className='min-h-screen'>Dashboard</div>
   )
